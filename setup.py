ccc3974bc642fcbda6acdd87f5e677f77b598e2a
Aman Agarwal <amanagr1804@gmail.com>
2025-03-28 19:37:06 +0530

    pypi changes

--- setup.py
+++ setup.py
@@ -3,18 +3,26 @@ from setuptools import setup, find_packages
 setup(
     name='minion-works', 
     version='0.0.1',
-    author='@aman, @cheena, @sai',
+    author='Aman, Cheena, Sai',
     author_email='sai@cobuild.tech',
-    description='Agents for Menial tasks',
-    long_description=open('README.md').read(),
+    description='Agents for menial tasks',
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/minionworks/minions', 
-    packages=find_packages(),  
+    url='https://github.com/minionworks/minions',
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Change license if needed
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    license='MIT',
     python_requires='>=3.7',
-    install_requires=open('requirements.txt').read().splitlines(),
+    install_requires=[
+        "asyncio",
+        "playwright",
+        "openai",
+        "markdownify",
+        "python-dotenv",
+        "langchain-core"
+    ],
 )
